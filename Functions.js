ff783767414e81eea1e62675aae9e9671064c8bb
Marcos Reymond <marcos.a.silva.99@gmail.com>
2024-07-20 13:52:45 -0300

    função mosquitos randômicos

--- Functions.js
+++ Functions.js
@@ -1,33 +1,38 @@
 var altura = 0
 var largura = 0
 
-function calcularAlturaLargura() {
-    largura = window.innerHeight
-    altura = window.innerWidth
+altura = window.innerHeight
+largura = window.innerWidth
 
-    console.log(largura, altura)
+//console.log(largura, altura)
+
+function posicaoRandomica() {
+
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+    
+    var posicaoX = Math.floor(Math.random() * largura) - 100
+    var posicaoY = Math.floor(Math.random() * altura) - 100
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    //console.log(posicaoX, posicaoY)
+
+    //criar o elemento html
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+
 }
-
-calcularAlturaLargura()
-
-var posicaoX = Math.floor(Math.random() * largura) - 100
-var posicaoY = Math.floor(Math.random() * altura) - 100
-
-posicaoX = posicaoX < 0 ? 0 : posicaoX;
-posicaoY = posicaoY < 0 ? 0 : posicaoY;
-
-//console.log(posicaoX, posicaoY)
-
-//criar o elemento html
-
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosca.png'
-mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
-
-document.body.appendChild(mosquito)
 
 //classe randomica de tamanho
 
@@ -57,5 +62,5 @@ function ladoAleatorio() {
         case 1:
             return 'ladoB'
     }
-} console.log(ladoAleatorio())
+} //console.log(ladoAleatorio())
 
