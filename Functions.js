29cebbe6fb8a0d37e50ce89edb48b12066e4be53
Marcos Reymond <marcos.a.silva.99@gmail.com>
2024-07-20 13:22:19 -0300

    Subindo os arquivos do projeto

---
+++ Functions.js
@@ -0,0 +1,61 @@
+var altura = 0
+var largura = 0
+
+function calcularAlturaLargura() {
+    largura = window.innerHeight
+    altura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+calcularAlturaLargura()
+
+var posicaoX = Math.floor(Math.random() * largura) - 100
+var posicaoY = Math.floor(Math.random() * altura) - 100
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX;
+posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+//console.log(posicaoX, posicaoY)
+
+//criar o elemento html
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+
+//classe randomica de tamanho
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+
+} //console.log(tamanhoAleatorio())
+
+function ladoAleatorio() {
+    var lado = Math.floor(Math.random() * 2);
+
+    switch(lado) {
+        case 0:
+            return 'ladoA'
+
+        case 1:
+            return 'ladoB'
+    }
+} console.log(ladoAleatorio())
+
